9df91e1d8098a99e5ae5ca63e4e232aa02517590
Diego Slva <contato@diegosilva.me>
2021-05-10 17:34:39 -0300

    Fixed a serious issue: smtp authentication information was readable within code, so this information has been moved to .env file, and now Email model uses env function to retrieve smtp auth data.

--- mailer/app/Models/Email.php
+++ mailer/app/Models/Email.php
@@ -18,12 +18,12 @@ class Email extends Model
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                       //Enable verbose debug output
         $mail->isSMTP();                                             //Send using SMTP
-        $mail->Host       = 'smtp.ipage.com';                        //Set the SMTP server to send through
+        $mail->Host       = env('MAIL_HOST');                        //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                    //Enable SMTP authentication
-        $mail->Username   = 'contato@movimentoforabolsonaro.com.br'; //SMTP username
-        $mail->Password   = 'R#1weNQsty5d';                          //SMTP password
+        $mail->Username   = end('MAIL_USERNAME'); //SMTP username
+        $mail->Password   = end('R#1weNQsty5d');                     //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;          //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;                                     //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        $mail->Port       = end("MAIL_PORT");                        //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         // Set From
         $mail->setFrom('contato@movimentoforabolsonaro.com.br', 'Movimento Fora Bolsonaro');
@@ -49,7 +49,7 @@ class Email extends Model
 
                  //Content
                 $mail->isHTML(true);                                  
-                $mail->Subject = 'Teste de robô!';
+                $mail->Subject = 'Teste de mensagem agendada!';
                 $mail->Body    = '<h1>Testando...</h1> <br/><br/><p>Foi <b>disparado</b> através de um script de envio agendado com sucesso!</p>';
                 $mail->AltBody = 'Testando...\n\nFoi disparado<através de um script de envio agendado com sucesso!';
             
