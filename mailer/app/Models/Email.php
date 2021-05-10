12b2451796a6a2f142324d89d7fff369150f5245
Diego Slva <contato@diegosilva.me>
2021-05-10 17:39:08 -0300

    Fixed mistakes on Email model

--- mailer/app/Models/Email.php
+++ mailer/app/Models/Email.php
@@ -20,10 +20,10 @@ class Email extends Model
         $mail->isSMTP();                                             //Send using SMTP
         $mail->Host       = env('MAIL_HOST');                        //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                    //Enable SMTP authentication
-        $mail->Username   = end('MAIL_USERNAME'); //SMTP username
-        $mail->Password   = end('R#1weNQsty5d');                     //SMTP password
+        $mail->Username   = env('MAIL_USERNAME'); //SMTP username
+        $mail->Password   = env('MAIL_PASSWORD');                     //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;          //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = end("MAIL_PORT");                        //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        $mail->Port       = env("MAIL_PORT");                        //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         // Set From
         $mail->setFrom('contato@movimentoforabolsonaro.com.br', 'Movimento Fora Bolsonaro');
